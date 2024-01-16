2683fc6fc018a10db02bf7a9d598ce397d81680f
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-16 17:07:36 +0600

    project onGoing

--- 1_Module-Getting_Started_With_React-Describing_the_UI/1.13_Project_Tutorial_Tic_Tac_Toe_Game/tic-tac-toe/src/App.jsx
+++ 1_Module-Getting_Started_With_React-Describing_the_UI/1.13_Project_Tutorial_Tic_Tac_Toe_Game/tic-tac-toe/src/App.jsx
@@ -1,6 +1,17 @@
-function Square({ value }) {
+import { useState } from "react";
+
+function Square() {
+  const [value, setValue] = useState(null);
+
+  function handleClick() {
+    setValue("X");
+  }
+
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
+    <button
+      className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg"
+      onClick={handleClick}
+    >
       {value}
     </button>
   );
@@ -9,17 +20,17 @@ function Square({ value }) {
 export default function Board() {
   return (
     <>
-      <div>
+      <div className="flex">
         <Square />
         <Square />
         <Square />
       </div>
-      <div>
+      <div className="flex">
         <Square />
         <Square />
         <Square />
       </div>
-      <div>
+      <div className="flex">
         <Square />
         <Square />
         <Square />
