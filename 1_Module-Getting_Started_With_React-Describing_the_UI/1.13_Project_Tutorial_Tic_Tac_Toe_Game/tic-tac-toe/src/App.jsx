7c9bbf8b7164b1c636cd1bc2ffe1e5a864c23718
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-16 12:49:49 +0600

    test

--- 1_Module-Getting_Started_With_React-Describing_the_UI/1.13_Project_Tutorial_Tic_Tac_Toe_Game/tic-tac-toe/src/App.jsx
+++ 1_Module-Getting_Started_With_React-Describing_the_UI/1.13_Project_Tutorial_Tic_Tac_Toe_Game/tic-tac-toe/src/App.jsx
@@ -1,7 +1,7 @@
-function Square() {
+function Square({ value }) {
   return (
     <button className="bg-white border border-gray-400 h-12 w-12 m-1 leading-9 text-lg">
-      X
+      {value}
     </button>
   );
 }
