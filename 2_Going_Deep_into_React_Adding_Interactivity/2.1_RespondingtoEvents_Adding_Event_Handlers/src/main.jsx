18d88b08dd827914021478500119e4c2698182b3
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-22 15:46:29 +0600

    understood Event Handling

---
+++ 2_Going_Deep_into_React_Adding_Interactivity/2.1_RespondingtoEvents_Adding_Event_Handlers/src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import Toolbar from './Toolbar.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Toolbar/>
+  </React.StrictMode>,
+)
