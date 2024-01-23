d3b61a54e364cf9df559018619e39b8a41607807
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-23 11:47:52 +0600

    dd

---
+++ 2_Going_Deep_into_React_Adding_Interactivity/2.2_EventPropagation/Event_Propagation_in_React/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return <>
+    <button className="border border-gray-400 p-3 m-3" onClick={(e) => {
+      console.log(e)
+  }}>Click It</button>
+  </>;
+}
+
+export default App;
---
+++ doc.md
@@ -0,0 +1,5 @@
+# Event Propagation in React:
+
+## Notes: 
+- If you want to prevent an event from reaching parent components, you need to call `e.stopPropagation()`
+- 
