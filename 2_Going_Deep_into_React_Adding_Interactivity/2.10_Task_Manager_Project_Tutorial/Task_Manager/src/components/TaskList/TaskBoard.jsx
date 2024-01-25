9870587f93f1644c3d4c124220977d6755ebb472
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-25 18:53:24 +0600

    d

--- 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/Task_Manager/src/components/TaskList/TaskBoard.jsx
+++ 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/Task_Manager/src/components/TaskList/TaskBoard.jsx
@@ -14,12 +14,14 @@ const TaskBoard = () => {
   };
   const [tasks, setTask] = useState([defaultTask]);
 
+  function handleAddTask() {}
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <SearchTask />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions />
+          <TaskActions onAddClick={handleAddTask} />
           <TaskList key={tasks.id} tasks={tasks} />
         </div>
       </div>
