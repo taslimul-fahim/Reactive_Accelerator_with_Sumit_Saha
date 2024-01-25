134c5ed33be6f0e361dab083f2d63d102a7b9217
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-25 19:15:50 +0600

    d

--- 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/Task_Manager/src/components/TaskList/TaskBoard.jsx
+++ 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/Task_Manager/src/components/TaskList/TaskBoard.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
+import AddTaskModal from "./AddTaskModal";
 
 const TaskBoard = () => {
   const defaultTask = {
@@ -13,15 +14,15 @@ const TaskBoard = () => {
     isFavorite: true,
   };
   const [tasks, setTask] = useState([defaultTask]);
-
-  function handleAddTask() {}
+  const [showAddModal, setShowAddModal] = useState(false);
 
   return (
     <section className="mb-20" id="tasks">
+      {showAddModal && <AddTaskModal/>}
       <div className="container">
         <SearchTask />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
-          <TaskActions onAddClick={handleAddTask} />
+          <TaskActions onAddClick={()=>setShowAddModal(true)} />
           <TaskList key={tasks.id} tasks={tasks} />
         </div>
       </div>
