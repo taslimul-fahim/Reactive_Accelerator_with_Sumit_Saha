fcb987cca2090a49a0c040acfe2e1b10bd76f04d
taslimulfahim <taslimulhasanfahim@gmail.com>
2024-01-25 18:19:43 +0600

    'd'

--- 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/Task_Manager/src/components/TaskList/TaskBoard.jsx
+++ 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/Task_Manager/src/components/TaskList/TaskBoard.jsx
@@ -1,15 +1,26 @@
+import { useState } from "react";
 import SearchTask from "./SearchTask";
 import TaskActions from "./TaskActions";
 import TaskList from "./TaskList";
 
 const TaskBoard = () => {
+  const defaultTask = {
+    id: crypto.randomUUID(),
+    title: "Learn React",
+    des: "I want to learn react and want to be an expert React developer.",
+    tags: ["web", "react", "js"],
+    priority: "High",
+    isFavorite: true,
+  };
+  const [tasks, setTask] = useState([defaultTask]);
+
   return (
     <section className="mb-20" id="tasks">
       <div className="container">
         <SearchTask />
         <div className="rounded-xl border border-[rgba(206,206,206,0.12)] bg-[#1D212B] px-6 py-8 md:px-9 md:py-16">
           <TaskActions />
-          <TaskList />
+          <TaskList key={tasks.id} tasks={tasks} />
         </div>
       </div>
     </section>
--- 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/doc.md
+++ 2_Going_Deep_into_React_Adding_Interactivity/2.10_Task_Manager_Project_Tutorial/doc.md
@@ -1,1 +1,6 @@
 # project Tutorial - Task Manager
+
+## Notes:
+
+- To create create random unique id by JavasCript: `crypto.randomUUID()`
+- 
